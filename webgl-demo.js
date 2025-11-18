2ef4a1232c2f55fed60c3a244fc75369b1aa9393
DatFrozenCube <167322680+DatFrozenCube@users.noreply.github.com>
2025-11-18 18:22:49 -0500

    Update webgl-demo.js

--- webgl-demo.js
+++ webgl-demo.js
@@ -35,69 +35,9 @@ function main() {
 `;
 
   const fsSource = `
-	uniform vec2 u_resolution;
-	uniform vec2 u_mouse;
-	uniform float u_time;
-	
-	float ndot(vec2 a, vec2 b ) { return a.x*b.x - a.y*b.y; }
-	float sdRhombus( in vec2 p, in vec2 b ) 
-	{
-	    p = abs(p);
-	    float h = clamp( ndot(b-2.0*p,b)/dot(b,b), -1.0, 1.0 );
-	    float d = length( p-0.5*b*vec2(1.0-h,1.0+h) );
-	    return d * sign( p.x*b.y + p.y*b.x - b.x*b.y );
-	}
-	
-	float sdUnevenCapsule( vec2 p, float r1, float r2, float h )
-	{
-	    p.x = abs(p.x);
-	    float b = (r1-r2)/h;
-	    float a = sqrt(1.0-b*b);
-	    float k = dot(p,vec2(-b,a));
-	    if( k < 0.0 ) return length(p) - r1;
-	    if( k > a*h ) return length(p-vec2(0.0,h)) - r2;
-	    return dot(p, vec2(a,b) ) - r1;
-	}
-	
-	vec3 palette( in float t, in vec3 a, in vec3 b, in vec3 c, in vec3 d )
-	{
-	    return a + b*cos( 6.283185*(c*t+d) );
-	}
-	
-	//[[0.500 0.500 0.500] [0.500 0.500 0.500] [0.800 0.800 0.500] [0.000 0.200 0.500]]
-	//[[0.000 0.500 0.500] [0.000 0.500 0.500] [0.000 0.333 0.500] [0.000 0.667 0.500]]
-	
-	void main() {
-	    vec2 uv = (gl_FragCoord.xy * 2. - u_resolution.xy) /u_resolution.y;
-	    vec2 uv0 = uv;
-	    vec2 uv1 = uv;
-		vec3 color;
-	    float box;
-	    float circle;
-	    float capsule;
-	    
-	    for (float i = 0.0; i < 3.0; i++){
-	        uv = fract(uv * 1.216) - 0.484;
-	        uv1 = fract(uv1 * 1.504 * sin(u_time*5.)) - 0.300;
-	        
-	        vec2 l = vec2(0.270,-0.270);
-	        vec2 d = abs(uv)-l;
-	        vec2 d1 = abs(uv1)-l;
-	
-	        vec3 a = vec3(0.0, 0.5, 0.5);
-	        vec3 b = vec3(0.0, 0.5, 0.5);
-	        vec3 c = vec3(0.0, 0.333, 0.5);
-	        vec3 d2 = vec3(0.0, 0.667, 0.5);
-	        
-	        d = 0.01 / d;
-	
-	        box = sdRhombus(d, l);
-	        capsule = sdUnevenCapsule(d1, 0.5, 0.5, 1.0);
-	        color = palette(length(uv0)+u_time, a, b, c, d2)*abs((sin(u_time)));
-	    }
-	
-	    gl_FragColor = vec4(color*box*capsule,1.0);
-	}
+	    void main() {
+      gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    }
   `;
 
   // Initialize a shader program; this is where all the lighting

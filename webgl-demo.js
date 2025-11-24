35361a27da759d68c74b5903894427bc3b16143e
DatFrozenCube <gogreenmesmer@hotmail.com>
2025-11-24 15:35:00 -0500

    Update webgl-demo.js

--- webgl-demo.js
+++ webgl-demo.js
@@ -31,24 +31,18 @@ function main() {
 
   const vsSource = `
     attribute vec4 aVertexPosition;
-    attribute vec4 aVertexColor;
 
     uniform mat4 uModelViewMatrix;
     uniform mat4 uProjectionMatrix;
 
-    varying lowp vec4 vColor;
-
     void main(void) {
       gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
-      vColor = aVertexColor;
     }
   `;
 
   // Fragment shader program
 
   const fsSource = `
-    varying lowp vec4 vColor;
-
     uniform mediump vec2 uResolution;
 
     float sdPentagon( in vec2 p, in float r )
